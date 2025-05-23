aec546bef000da128aaf4a0f032b8fecf7a462c3
uncle_charlie <sharp.bruce.lee@gmail.com>
2025-05-23 16:30:55 +0800

    scan camera ready

--- app/scanner.tsx
+++ app/scanner.tsx
@@ -1,44 +1,42 @@
 import { useRouter } from "expo-router";
 import { useEffect } from "react";
-import { SafeAreaView, Text, View } from "react-native";
-import { useCameraPermission } from "react-native-vision-camera";
+import { SafeAreaView, StyleSheet } from "react-native";
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+} from "react-native-vision-camera";
 
 export default function Scanner() {
   const router = useRouter();
   const { hasPermission, requestPermission } = useCameraPermission();
+
+  const device = useCameraDevice("back");
 
   useEffect(() => {
     (async () => {
       if (!hasPermission) {
         const status = await requestPermission();
         if (!status) {
-          router.push("/");
+          router.push("/permission");
         }
       }
     })();
   }, []);
 
+  // useEffect(() => {
+  //   if (!device) {
+  //     console.log(device);
+  //   }
+  // }, [device]);
+
+  if (!device) return <div>No device</div>;
+
   return (
     <SafeAreaView
       style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
     >
-      <View
-        style={{
-          backgroundColor: "#f0f0f0",
-          padding: 20,
-          borderRadius: 10,
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 24,
-            fontWeight: "bold",
-            color: "#007AFF",
-          }}
-        >
-          Scanner
-        </Text>
-      </View>
+      <Camera style={StyleSheet.absoluteFill} device={device} isActive />
     </SafeAreaView>
   );
 }
